bdeafe1f86c66aea8d9f1784f20afeaa689a40ec
Alejandro Encalado <aencalado@wibeee.com>
2021-03-07 10:32:29 +0100

    fix: removed learning test from index.spec.js

--- test/index.spec.js
+++ test/index.spec.js
@@ -139,32 +139,6 @@ describe('parse-xbrl', function (done) {
     });
   });
 
-  it('Learning test', async function (done) {
-    const documents = [
-      './test/sampleXbrlDocuments/xbrls/2019/aapl/xml_0.xml',
-      './test/sampleXbrlDocuments/xbrls/2020/aapl/xml_0.xml',
-      './test/sampleXbrlDocuments/xbrls/2019/msft/xml_0.xml',
-      './test/sampleXbrlDocuments/xbrls/2020/msft/xml_0.xml',
-      './test/sampleXbrlDocuments/xbrls/2019/tsla/xml_0.xml',
-      './test/sampleXbrlDocuments/xbrls/2020/tsla/xml_0.xml'
-    ];
-
-    for (const doc of documents) {
-      try {
-        const filing = await ParseXbrl.parse(doc);
-        const [, , , , year, company] = doc.split('/');
-        console.log({ year, company });
-        await fs.promises.writeFile(
-          path.dirname(doc) + path.sep + company + '_' + year + '.json',
-          JSON.stringify(filing, null, 2)
-        );
-      } catch (ex) {
-        console.error(ex);
-      }
-    }
-    done();
-  });
-
   it('can load EntityRegistrantName', function (done) {
     const oldData = fs.readFileSync(
       './test/sampleXbrlDocuments/ruby_tuesday_10q.xml',
